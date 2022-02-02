198d13c8515ee7cc12d010bf2dbb549ae3d369b2
en30 <en30.git@gmail.com>
2022-02-02 10:21:40 +0900

    LIVEBOOK_HOST env var

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const http = require("http"),
   httpProxy = require("http-proxy");
 
+const LIVEBOOK_HOST = process.env.LIVEBOOK_HOST;
+
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
   if (!req.headers.cookie?.includes("ok=1")) {
     return res.writeHead(403, {}).end("blocked");
   }
 
-  proxy.web(req, res, { target: "http://livebook:8080" });
+  proxy.web(req, res, { target: `http://${LIVEBOOK_HOST}:8080` });
 });
 server.on("upgrade", (req, socket, head) => {
-  proxy.ws(req, socket, head, { target: "ws://livebook:8080" });
+  proxy.ws(req, socket, head, { target: `ws://${LIVEBOOK_HOST}:8080` });
 });
 
 server.listen(3000);
