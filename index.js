c8339b8dd36a89af491a8a6d913d502237d04915
en30 <en30.git@gmail.com>
2022-02-02 10:22:08 +0900

    Add more log output

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const LIVEBOOK_HOST = process.env.LIVEBOOK_HOST;
 const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
   if (!req.headers.cookie?.includes("ok=1")) {
+    console.warn(`[blocked] ${req.method}: ${req.url}`);
+    console.warn(req.headers);
     return res.writeHead(403, {}).end("blocked");
   }
 
+  console.log(`[accepted] ${req.method}: ${req.url}`);
   proxy.web(req, res, { target: `http://${LIVEBOOK_HOST}:8080` });
 });
 server.on("upgrade", (req, socket, head) => {
